f22e114807a56f01c1090c08f247c3b457248ef2
John Hayes <hayesjohn54@yahoo.com>
2022-06-07 18:40:28 -0400

    commit tree f3e20250a8ae8ebfa6b1c490709d8031058afd21

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+//    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+//    
+//}
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+        
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);*/
+
+
+function writeCards(arr, ryan){
+    let finishedCards = [];
+    for (let i = 0; i < arr.length; i++){
+        finishedCards.push(`Thank you, ${arr[i]}, for the wonderful ${ryan} gift!`);
+    }
+    return finishedCards;
+}
+
+function countDown(number){
+    let i = 10;
+    while (i >= 0){
+        console.log(i--);
+    
+    }
+}
